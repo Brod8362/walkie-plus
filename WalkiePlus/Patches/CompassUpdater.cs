16f5a13d4592265d2bc66163fc942c04b6e618a1
Brod8362 <brod8362@gmail.com>
2023-11-05 14:25:55 -0600

    Refactor compass logic

--- WalkiePlus/Patches/CompassUpdater.cs
+++ WalkiePlus/Patches/CompassUpdater.cs
@@ -12,32 +12,42 @@ public class CompassUpdater
 
     public static void CompassUpdate()
     {
-        GameObject compassParent = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner/Compass");
-        compassParent.SetActive(true);
-        GameObject textContainer = GameObject.Find(
-            "Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner/Compass/Container/CardinalDirections/North"
-        );
+        if (StartOfRound.Instance is null)
+            return;
+        
+        PlayerControllerB playerController = StartOfRound.Instance.localPlayerController;
 
-        if (textContainer != null)
+        if (playerController is null)
+            return;
+        
+        GameObject compassParent = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner/Compass");
+        if (compassParent is null)
+            return;
+
+        bool playerHoldingRadio = true;
+        
+        if (playerHoldingRadio)
         {
+            GameObject textContainer = GameObject.Find(
+                "Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner/Compass/Container/CardinalDirections/North"
+            );
+            if (textContainer is null)
+                return;
             TextMeshProUGUI directionText = textContainer.GetComponent<TextMeshProUGUI>();
-            if (directionText != null)
-            {
-                if (StartOfRound.Instance != null)
-                {
-                    PlayerControllerB playerController = StartOfRound.Instance.localPlayerController;
-                    if (playerController != null)
-                    {
-                        Vector3 eulerRotation = playerController.playerGlobalHead.rotation.eulerAngles;
-                        double axisRotation = eulerRotation.y;
-                        int snappedAngle = (int)(Math.Round(axisRotation / 90.0) * 90.0) % 360;
-                        //results of this are 0, 90, 180, 270
-                        int snappedAngleIndex = snappedAngle / 90;
-                        directionText.text = Directions[snappedAngleIndex];
-                    }
-                    
-                }
-            }
+            if (directionText is null)
+                return;
+            
+            Vector3 eulerRotation = playerController.playerGlobalHead.rotation.eulerAngles;
+            double axisRotation = eulerRotation.y;
+            int snappedAngle = (int)(Math.Round(axisRotation / 90.0) * 90.0) % 360;
+            //results of this are 0, 90, 180, 270
+            int snappedAngleIndex = snappedAngle / 90;
+            directionText.text = Directions[snappedAngleIndex];
         }
+        else
+        {
+            compassParent.SetActive(false);
+        }
+
     }
 }
